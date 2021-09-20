b41fbde35e98799d88cd5ca06cb21e7d87bd6a32
ThiagoYure <thigoyure@gmail.com>
2021-09-20 12:17:10 -0300

    Adicionando modulo de CSS para componentes

--- exemplo-nextjs/pages/champions.js
+++ exemplo-nextjs/pages/champions.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Styles from '../styles/ListPages.module.css'
 
 function ChampionsPage({ champions }) {
     console.log(Object.keys(champions));
@@ -7,17 +9,31 @@ function ChampionsPage({ champions }) {
         championsList.push(champions[champ]);
     });
     return (
-        <ul>
-            {championsList.map((champ) => {
-                return (
-                    <li key={champ["id"]}>
+        <div className={Styles.container}>
+            <h1 className={Styles.title}>Todos os Campeões</h1>
+            <h3 className={Styles.description}>Escolha sabiamente...</h3>
+            <ul className={Styles.result}>
+                {championsList.map((champ) => {
+                    return (
                         <Link href={`/champions/${champ["id"]}`}>
-                            <a>{champ["name"]}</a>
+                            <a className={Styles.card}>
+                                <li key={champ["id"]}>
+                                    <Image
+                                        src={`http://ddragon.leagueoflegends.com/cdn/11.18.1/img/champion/${champ["image"]["full"]}`}
+                                        alt="Picture of the champion"
+                                        width={100}
+                                        height={100}
+                                        blurDataURL="data:..."
+                                        placeholder="blur" 
+                                    />
+                                    <p>{champ["name"]}, {champ["title"]}</p>
+                                </li>
+                            </a>
                         </Link>
-                    </li>
-                )
-            })}
-        </ul>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
